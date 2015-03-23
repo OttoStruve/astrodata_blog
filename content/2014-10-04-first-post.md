9cb942a03f297daff1f4dfdf772d99741725eef7
Jacob Hummel <jhummel@astro.as.utexas.edu>
2015-03-23 17:49:43 -0600

    Setting up blog.  Something missing, but can't figure it out in the airport without wifi.

---
+++ content/2014-10-04-first-post.md
@@ -0,0 +1,8 @@
+Title: First Post
+date: 2014-10-04 05:00
+comments: false
+slug: first-post
+
+Hello World!
+-----------------------
+First post for my [blog](http://hummel.github.io). Let's do this!
---
+++ publishconf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+import sys
+sys.path.append('.')
+from pelicanconf import *
+
+# keep the .com address to properly find disqus comments
+SITEURL = 'http://OttoStruve.github.io'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+
+# Uncomment following line for absolute URLs in production:
+#RELATIVE_URLS = False
+
+#GOOGLE_ANALYTICS = ''
+#DISQUS_SITENAME = ''
